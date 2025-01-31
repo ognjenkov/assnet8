2ac7035991da2a4c77078c5b90c15c639b476052
ognjenkov <ognjen.mobilni2407@gmail.com>
2025-01-31 08:43:36 +0100

    model change

--- Models/Entry.cs
+++ Models/Entry.cs
@@ -8,12 +8,12 @@ namespace assnet8.Models
     public class Entry
     {
         public Guid Id { get; set; } = Guid.NewGuid();
-        public Guid UserId { get; set; } // jedan prema vise zato on pamti guid
-        public Guid GameId { get; set; }
         public DateTime CreateDateTime { get; set; } = DateTime.UtcNow;
         public int OpNumber { get; set; }
         public int RentNumber { get; set; }
         public string? Message { get; set; }
+        public Guid UserId { get; set; } // jedan prema vise zato on pamti guid
+        public Guid GameId { get; set; }
         public User? User { get; set; }
         public Game? Game { get; set; }
     }
--- Models/Team.cs
+++ Models/Team.cs
@@ -11,9 +11,11 @@ namespace assnet8.Models
         public string Name { get; set; } = string.Empty;
         public DateTime CreateDateTime { get; set; } = DateTime.UtcNow;
         public Guid? LogoImageId { get; set; } //jedan prema jedan zavisi od fielda tima itd.. zato ona nosi guid njihov
+        public Guid? LocationId { get; set; } //jedan prema jedan zavisi od fielda tima itd.. zato ona nosi guid njihov
         public List<Gallery>? Galleries { get; set; }
         public Organization? Organization { get; set; } // u slucaju da tim ima organizaciju prvno nastaje tim - zato je guid u orgu
         public Image? LogoImage { get; set; }
         public List<Membership>? Memberships { get; set; }
+        public Location? Location { get; set; }
     }
 }
